bcf61dafe6ae0165616bb978090dccb92f135c62
JeffJohnsonPrimedia <jeff.johnson@primedia.com>
2012-03-12 15:23:36 -0400

    this is the new commit message without passphrase

--- commit2git.bat
+++ commit2git.bat
@@ -1,6 +1,5 @@
 ﻿call "C:\Program Files (x86)\Git\cmd\git.cmd" config --global user.name "JeffJohnsonPrimedia"
 call "C:\Program Files (x86)\Git\cmd\git.cmd" config --global user.email "jeff.johnson@primedia.com"
-call "C:\Program Files (x86)\Git\cmd\git.cmd" commit --amend --reset-author
 call "C:\Program Files (x86)\Git\cmd\git.cmd" add commit2git.bat
 call "C:\Program Files (x86)\Git\cmd\git.cmd" add sample6.txt
 call "C:\Program Files (x86)\Git\cmd\git.cmd" commit -m "this is the new commit message without passphrase"
